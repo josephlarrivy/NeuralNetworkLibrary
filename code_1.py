6fe45f9e88e3de197e0eabf19b6d37aa55910573
josephlarrivy <joseph.larrivy@gmail.com>
2023-12-13 20:42:38 -0600

    picking back up

--- code_1.py
+++ code_1.py
@@ -1,7 +1,8 @@
-import sys
-import numpy as np
-import matplotlib
-
+# import sys
+# import numpy as np
+# import matplotlib
+# import nnfs
+# import matplotlib.pyplot as plt
 
 
 
@@ -76,15 +77,100 @@ import matplotlib
 # ########################################
 
 
-# deep learning often uses X as the vaiable for the data set (inputs)
-# 3 samples here
-X = [[1,2,3,2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
+# # deep learning often uses X as the vaiable for the data set (inputs)
+# # 3 samples here
+# X = [[1,2,3,2.5],
+#     [2.0, 5.0, -1.0, 2.0],
+#     [-1.5, 2.7, 3.3, -0.8]]
 
-# if there is a trained model, that data would be used to initialize - otherwise we randomize before we have a trained model
+# # if there is a trained model, that data would be used to initialize - otherwise we randomize before we have a trained model
+
+# # np.random.seed(0)
+
+# class Layer_Dense:
+#     def __init__(self, n_inputs, n_neurons):
+#         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+#         self.biases = np.zeros((1, n_neurons))
+
+#     def forward(self, inputs):
+#         inputs = np.array(inputs)  # Convert inputs to a NumPy array
+#         self.output = np.dot(inputs, self.weights) + self.biases
+
+# layer1 = Layer_Dense(4, 10)
+# layer2 = Layer_Dense(10, 10)
+# layer3 = Layer_Dense(10, 10)
+# layer4 = Layer_Dense(10, 2)
+
+# layer1.forward(X)
+# # print(layer1.output)
+# layer2.forward(layer1.output)
+# layer3.forward(layer2.output)
+# layer4.forward(layer3.output)
+# print(layer4.output)
+
+
+# ########################################
+
+
+# inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+# output = []
+
+# for i in inputs:
+#     if i > 0:
+#         output.append(i)
+#     elif i <= 0:
+#         output.append(0)
+
+# print(output)
+
+import sys
+import numpy as np
+import matplotlib
+import nnfs
+import matplotlib.pyplot as plt
+
+nnfs.init()
+
+X = np.array([[1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]])
+
 class Layer_Dense:
-    def __init__(self):
-        pass
-    def forward(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(4, 5)  # Adjust the number of inputs to match the number of features in your dataset
+activation1 = Activation_ReLU()
+
+layer1.forward(X)  # Convert X to a list and pass it to forward method
+
+activation1.forward(layer1.output)
+
+print(layer1.output)
+
+
+# this function create the spiral dataset
+# def spiral_data(points, classes):
+#     X = np.zeros((points*classes, 2))
+#     y = np.zeros(points*classes, dtype='uint8')
+#     for class_number in range(classes):
+#         ix = range(points*class_number, points*(class_number+1))
+#         r = np.linspace(0.0, 1, points)  # radius
+#         t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+#         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+#         y[ix] = class_number
+#     return X, y
+
+# print('here')
+# X, y = spiral_data(1000, 2)
+
+# plt.scatter(X[:,0], X[:,1])
+# plt.show()
